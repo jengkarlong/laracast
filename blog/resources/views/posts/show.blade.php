ae94f2bb7cfb133eb9dfab06455f0eccbadddc5f
Jen <jengkarlong@gmail.com>
2018-02-12 21:58:47 +0800

    update laravel blog app

--- blog/resources/views/posts/show.blade.php
+++ blog/resources/views/posts/show.blade.php
@@ -2,6 +2,13 @@
 
 @section ('content')
 
-    qweqwe
+    <div class="col-md-8 blog-main">
+        
+        <h1>{{ $post->title }}</h1>
+
+        {{ $post->body }}
+
+
+    </div>
 
 @endsection
--- blog/routes/web.php
+++ blog/routes/web.php
@@ -12,7 +12,10 @@
 */
 
 Route::get('/', 'PostsController@index');
-// Route::get('/posts/{post}', 'PostsController@show');
-Route::get('/posts/{create}', 'PostsController@create');
+
+Route::get('/posts/create', 'PostsController@create');
 
 Route::post('/posts', 'PostsController@store');
+
+Route::get('/posts/{post}', 'PostsController@show');
+
